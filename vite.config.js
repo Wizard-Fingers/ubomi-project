a81e301fd5bcadf41a7c0ab807730e1724de9ab0
Repo <art@beardigitalstudios.com>
2023-12-02 09:14:12 +0000

    more changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ubomi-project",
+  // base: "/ubomi-project",
   // make it run on port 3000
   server: {
     port: 3000,
